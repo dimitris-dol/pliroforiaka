1d7adb40344479656c978a69406db9bc8ed66298
dimitris-dol <jimmdologlou@gmail.com>
2021-03-07 20:16:23 +0200

    remove unnecessary files

--- BackEnd/src/main/java/com/example/Networks/Exceptions/CustomException.java
+++
@@ -1,8 +0,0 @@
-package com.example.Networks;
-
-public class CustomException
-        extends RuntimeException {
-    public CustomException(String errorMessage) {
-        super(errorMessage);
-    } //test didnt work
-}
